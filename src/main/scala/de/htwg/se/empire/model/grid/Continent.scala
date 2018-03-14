536b20794c059dfa139f30144dac27fcf847abc4
Lukas Frey <lfy@sybit.de>
2018-03-14 16:34:42 +0100

    Add Test

--- src/main/scala/de/htwg/se/empire/model/grid/Continent.scala
+++ src/main/scala/de/htwg/se/empire/model/grid/Continent.scala
@@ -2,5 +2,5 @@ package de.htwg.se.empire.model.grid
 
 case class Continent(name: String, bonus: Int, countries: List[Country]) {
 
-  override def toString: String = name + " => " + "Bonus: " + bonus + " Countries: [" + countries.mkString(",") + "]"
+  override def toString: String = name + " => " + "Bonus: " + bonus + ", Countries: [" + countries.mkString(",") + "]"
 }
