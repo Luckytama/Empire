8142743512ce53ac227a55a7a451e97cbb24cc1d
Lukas Frey <lukas.lauchringen@googlemail.com>
2018-03-28 13:55:48 +0200

    Add Exception and log error

--- src/main/scala/de/htwg/se/empire/model/grid/Country.scala
+++ src/main/scala/de/htwg/se/empire/model/grid/Country.scala
@@ -1,11 +1,18 @@
 package de.htwg.se.empire.model.grid
 
+import org.apache.logging.log4j.{ LogManager, Logger }
+
 case class Country(name: String, adjacentCountries: List[Country]) {
+
+  val LOG: Logger = LogManager.getLogger(this.getClass)
 
   var soldiers = 0
 
   def addSoldiers(numberOfSoldiers: Int): Unit = {
-    assert(0 < numberOfSoldiers, "Numbers of soldiers can't be negative or null")
+    if (0 > numberOfSoldiers) {
+      LOG.error("Numbers of soldiers can't be negative or null")
+      throw new IllegalArgumentException
+    }
     soldiers += numberOfSoldiers
   }
 
