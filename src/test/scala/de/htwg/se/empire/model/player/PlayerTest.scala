8cc2ce621e93ccc844ffded1a881e569f4171f87
Manuel Haug <kontakt@manuel-haug.de>
2018-04-13 12:04:31 +0200

    Added player model

---
+++ src/test/scala/de/htwg/se/empire/model/player/PlayerTest.scala
@@ -0,0 +1,44 @@
+package de.htwg.se.empire.model.player
+
+import de.htwg.se.empire.model.grid.Country
+import org.junit.runner.RunWith
+import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.mutable.ListBuffer
+
+@RunWith(classOf[JUnitRunner])
+class PlayerTest extends WordSpec with Matchers {
+  "A Player" when {
+    val countries = ListBuffer[Country](Country("Another Country", null))
+    val player = Player("Player1", countries)
+    "new" should {
+      "have a name" in {
+        player.name should be("Player1")
+      }
+      "have an country" in {
+        player.countries.length should be(1)
+      }
+      "have a nice String representation" in {
+        player.toString should be("Player1")
+      }
+    }
+    "a country is added" should {
+      "have 2 countries" in {
+        player.addCountry(Country("New Country", null))
+        player.countries should be(ListBuffer(Country("Another Country", null), Country("New Country", null)))
+      }
+    }
+    "a country is removed" should {
+      "have 1 country" in {
+        player.removeCountry(Country("New Country", null))
+        player.countries should be(ListBuffer(Country("Another Country", null)))
+      }
+    }
+    "the country amount is requested" should {
+      "be 1 country" in {
+        player.getCountryAmount() should be(1)
+      }
+    }
+  }
+}
