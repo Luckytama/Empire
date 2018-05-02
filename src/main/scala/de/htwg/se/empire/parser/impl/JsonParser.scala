9ef90c8ac324a2a567bf1e59108f3b5cefdfaf89
Lukas Frey <lukas.frey@sybit.de>
2018-05-02 14:51:57 +0200

    Add Exception

--- src/main/scala/de/htwg/se/empire/parser/impl/JsonParser.scala
+++ src/main/scala/de/htwg/se/empire/parser/impl/JsonParser.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.empire.parser.impl
 
+import java.io.FileNotFoundException
+
 import de.htwg.se.empire.model.grid.PlayingField
 import de.htwg.se.empire.parser.Parser
 import org.json4s._
@@ -11,6 +13,7 @@ class JsonParser extends Parser {
 
   implicit val formats: DefaultFormats.type = DefaultFormats
 
+  @throws(classOf[FileNotFoundException])
   override def parseFile(path: String): PlayingField = {
     val source: String = Source.fromFile(path).getLines().mkString
     parse(source).extract[PlayingField]
