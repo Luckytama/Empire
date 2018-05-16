971108c50ee5e814cb41c5dc4264958a4986b635
Lukas Frey <lukas.frey@sybit.de>
2018-05-16 17:34:20 +0200

    Construct for distributing soldiers on init

--- src/main/scala/de/htwg/se/empire/controller/InitController.scala
+++ src/main/scala/de/htwg/se/empire/controller/InitController.scala
@@ -13,6 +13,11 @@ class InitController {
 
   val LOG: Logger = LogManager.getLogger(this.getClass)
 
+  val INIT_SOLDIERS_5PLAYER = 25
+  val INIT_SOLDIERS_4PLAYER = 30
+  val INIT_SOLDIERS_3PLAYER = 35
+  val INIT_SOLDIERS_2PLAYER = 40
+
   def setUpGrid(pathToGrid: String, players: String*): Option[PlayingField] = {
     val parser = new JsonParser
     try {
@@ -29,7 +34,10 @@ class InitController {
     }
   }
 
-  def randDistributeSoldiers(playingField: PlayingField): Unit = {
+  /*
+   * Distribute randomly all countries to all player with one soldiers in it
+   */
+  def randDistributeCountries(playingField: PlayingField): Unit = {
     if (playingField == null) {
       LOG.info("There is no playing field set yet")
       return
@@ -44,6 +52,33 @@ class InitController {
     } else {
       LOG.info("There are to less players to start the game")
     }
+  }
+
+  /*
+   * Distribute soldiers
+   * 5 Players: 25, 4 Players: 30, 3 Players: 35, 2 Players: 40
+   */
+  def randDistributeSoldiers(playingField: PlayingField): Unit = {
+    playingField.players.length match {
+      case 5 =>
+        distribute(playingField, INIT_SOLDIERS_5PLAYER)
+      case 4 =>
+        distribute(playingField, INIT_SOLDIERS_4PLAYER)
+      case 3 =>
+        distribute(playingField, INIT_SOLDIERS_3PLAYER)
+      case 2 =>
+        distribute(playingField, INIT_SOLDIERS_2PLAYER)
+      case _ =>
+        LOG.info("There are not a valid number of players")
+    }
+  }
+
+  def distribute(playingField: PlayingField, soldiers: Int): Unit = {
+    playingField.players.foreach(p => distributeForPlayer(p, soldiers))
+  }
+
+  def distributeForPlayer(player: Player, soldiers: Int): Unit = {
+
   }
 
   def splitList[T](l: List[T], pieces: Int, len: Int = -1, done: Int = 0, result: List[List[T]] = Nil): List[List[T]] = {
