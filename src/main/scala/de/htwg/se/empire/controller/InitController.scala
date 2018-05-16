27b4894d8b41f15a1e2245b1f4901db37240377f
Lukas Frey <lukas.frey@sybit.de>
2018-05-16 11:30:45 +0200

    Impl distribution of countries

--- src/main/scala/de/htwg/se/empire/controller/InitController.scala
+++ src/main/scala/de/htwg/se/empire/controller/InitController.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.empire.controller
 
-import de.htwg.se.empire.model.grid.{Country, PlayingField}
+import de.htwg.se.empire.model.grid.PlayingField
 import de.htwg.se.empire.model.player.Player
 import de.htwg.se.empire.parser.impl.JsonParser
 import org.apache.logging.log4j.{LogManager, Logger}
@@ -22,7 +22,7 @@ class InitController {
     val allCountries = playingField.getAllCountries
     val shuffeldCountries = Random.shuffle(allCountries)
     if (1 <= playingField.players.length) {
-      val playerCountries = shuffeldCountries.grouped(shuffeldCountries.length / playingField.players.length).toList zip playingField.players
+      val playerCountries = splitList(shuffeldCountries, playingField.players.length) zip playingField.players
       print(playerCountries)
       for ((cList, p) <- playerCountries) {
         cList.foreach(c => p.addCountry(c))
@@ -33,7 +33,14 @@ class InitController {
     }
   }
 
-  def split_list(countryList: List[Country], numberOfPlayer: Int) = {
-    //countryList.span()
+  def splitList[T](l: List[T], pieces: Int, len: Int = -1, done: Int = 0, result: List[List[T]] = Nil): List[List[T]] = {
+    if (l.isEmpty) {
+      result.reverse
+    }
+    else {
+      val n = if (len < 0) l.length else len
+      val ls = l.splitAt((n.toLong * (done + 1) / pieces - n.toLong * done / pieces).toInt)
+      splitList(ls._2, pieces, n, done + 1, ls._1 :: result)
+    }
   }
 }
