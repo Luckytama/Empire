94bba3c0dbbe782fbebec05df887f839380783b8
Lukas Frey <lukas.frey@sybit.de>
2018-06-11 18:16:10 +0200

    set bonus for conquer a whole continent

--- src/main/scala/de/htwg/se/empire/controller/impl/DefaultReinforcementController.scala
+++ src/main/scala/de/htwg/se/empire/controller/impl/DefaultReinforcementController.scala
@@ -13,8 +13,19 @@ class DefaultReinforcementController extends ReinforcementController {
    * Player gets one soldier for every 3 countries he have, but minimum 3
    */
   //TODO: savely remove playingfield from method
-  def calcSoldiersToDistribute(player: Player): Int = {
-    if (player.countries.length / 3 > 3) player.countries.length / 3 else 3
+  def calcSoldiersToDistribute(playingField: PlayingField, player: Player): Int = {
+    val bonus = getBonus(playingField, player)
+    if (player.countries.length / 3 > 3) (player.countries.length / 3) + bonus else 3 + bonus
+  }
+
+  def getBonus(playingField: PlayingField, player: Player): Int = {
+    var bonus = 0
+    for (c <- playingField.continents) {
+      val continentCountries = c.countries.toSet
+      val playerCountries = player.countries.toSet
+      if (playerCountries.subsetOf(continentCountries)) bonus += c.bonus
+    }
+    bonus
   }
 
   def distributeSoldiers(playingField: PlayingField, countryName: String, soldiers: Int): Unit = {
